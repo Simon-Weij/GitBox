378218d53372796c1f14f2d4273992cbdf7deab9
Simon <175155691+Simon-Weij@users.noreply.github.com>
2025-12-14 16:33:14 +0100

    Served svelte from gin

--- src/main.go
+++ src/main.go
@@ -9,10 +9,17 @@ import (
 func main() {
 	r := gin.Default()
 
-	r.GET("/ping", func(c *gin.Context) {
+	r.GET("/api/ping", func(c *gin.Context) {
 		c.JSON(http.StatusOK, gin.H{
 			"message": "pong",
 		})
 	})
+
+	r.Static("/_app", "./frontend/build/_app")
+
+	r.NoRoute(func(c *gin.Context) {
+		c.File("./frontend/build/index.html")
+	})
+
 	r.Run()
 }
